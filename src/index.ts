80379dfe15855d22934306475b5be09607aeafa1
cychann <skcbdlhc129@naver.com>
2024-12-04 17:01:39 +0900

    [Docs]: Add TSDoc comments to Quote class for better documentation

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,24 @@ import { make } from "@editorjs/dom";
 import type { API, BlockAPI, BlockTool, ToolConfig } from "@editorjs/editorjs";
 import { MenuConfig } from "@editorjs/editorjs/types/tools";
 
+/**
+ * Configuration interface for the Quote tool.
+ */
 export interface QuoteConfig extends ToolConfig {
   defaultType: QuoteType;
 }
 
+/**
+ * Data structure for the Quote block.
+ */
 export interface QuoteData {
   text: string;
   type: QuoteType;
 }
 
+/**
+ * Parameters for the Quote constructor.
+ */
 interface QuoteParams {
   data: QuoteData;
   config: QuoteConfig;
@@ -22,6 +31,9 @@ interface QuoteParams {
   block: BlockAPI;
 }
 
+/**
+ * CSS class names used in the Quote block.
+ */
 interface QuoteCSS {
   baseClass: string;
   wrapper: string;
@@ -29,12 +41,18 @@ interface QuoteCSS {
   text: string;
 }
 
+/**
+ * Enum for the different types of quotes.
+ */
 enum QuoteType {
   QuotationMark = "quotationMark",
   VerticalLine = "verticalLine",
   Box = "box",
 }
 
+/**
+ * Quote class representing a customizable quote block for Editor.js.
+ */
 export default class Quote implements BlockTool {
   api: API;
   readOnly: boolean;
@@ -44,6 +62,10 @@ export default class Quote implements BlockTool {
   private _CSS: QuoteCSS;
   private _quoteElement!: HTMLElement;
 
+  /**
+   * Creates an instance of the Quote block.
+   * @param params - The parameters for the Quote block.
+   */
   constructor({ data, config, api, readOnly, block }: QuoteParams) {
     const { DEFAULT_TYPE } = Quote;
 
@@ -75,7 +97,7 @@ export default class Quote implements BlockTool {
   static get toolbox() {
     return {
       icon: IconQuote,
-      title: "인용구",
+      title: "Quote",
     };
   }
 
@@ -88,7 +110,7 @@ export default class Quote implements BlockTool {
   }
 
   static get DEFAULT_TYPE(): QuoteType {
-    return QuoteType.Type1;
+    return QuoteType.QuotationMark;
   }
 
   static get conversionConfig() {
@@ -101,6 +123,9 @@ export default class Quote implements BlockTool {
     };
   }
 
+  /**
+   * Gets the CSS class names for the quote block.
+   */
   get CSS(): QuoteCSS {
     return {
       baseClass: this.api.styles.block,
@@ -110,26 +135,33 @@ export default class Quote implements BlockTool {
     };
   }
 
+  /**
+   * Gets the settings for the quote block.
+   */
   get settings(): { name: QuoteType; label: string; icon?: string }[] {
     return [
       {
         name: QuoteType.QuotationMark,
-        label: "Quote Type 1",
+        label: "Quote Mark",
         icon: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="editor-icon"><path id="Vector" d="M13.4831 16L12.9348 15.2444C13.6348 14.7628 14.2457 14.168 14.7411 13.4856C15.1102 12.9691 15.3133 12.3568 15.3244 11.7268L15.3244 11.5341C15.1838 11.5523 15.0551 11.5683 14.9383 11.5821C14.8287 11.5955 14.7184 11.6026 14.6079 11.6032C14.379 11.6097 14.1512 11.5704 13.9384 11.4876C13.7256 11.4048 13.5324 11.2803 13.3708 11.1218C13.2095 10.9659 13.0826 10.7794 12.9979 10.5738C12.9132 10.3682 12.8725 10.1478 12.8783 9.92622C12.8654 9.67057 12.9047 9.41498 12.994 9.17437C13.0833 8.93376 13.2207 8.71295 13.3983 8.52485C13.5784 8.34918 13.7938 8.21199 14.031 8.12199C14.2682 8.03198 14.5219 7.99112 14.7761 8.00199C15.0814 7.99561 15.3842 8.05802 15.6609 8.18438C15.9376 8.31074 16.1809 8.49766 16.3718 8.73067C16.8042 9.25948 17.0264 9.92326 16.9975 10.5996C16.9991 11.6387 16.6913 12.6558 16.1115 13.5267C15.4258 14.5249 14.5296 15.3682 13.4831 16ZM8.57836 16L8.03081 15.2444C8.73082 14.7628 9.34168 14.168 9.83712 13.4856C10.2062 12.9691 10.4093 12.3568 10.4204 11.7268L10.4204 11.5341C10.2894 11.5523 10.1652 11.5683 10.0477 11.5821C9.93338 11.5959 9.81836 11.6029 9.70321 11.6032C9.47579 11.612 9.24899 11.5743 9.03726 11.4927C8.82554 11.4111 8.63353 11.2872 8.47347 11.129C8.31679 10.9694 8.1944 10.7808 8.11352 10.5742C8.03264 10.3676 7.99491 10.1473 8.00255 9.9262C7.97599 9.41221 8.15775 8.90869 8.50843 8.52483C8.68516 8.34862 8.89803 8.21093 9.13304 8.12082C9.36806 8.03072 9.61992 7.99022 9.87208 8.00199C10.1793 7.99665 10.4838 8.05945 10.7626 8.18565C11.0414 8.31186 11.2874 8.4982 11.482 8.73067C11.9225 9.25585 12.1503 9.92102 12.1218 10.5996C12.1182 11.6281 11.808 12.6333 11.229 13.4922C10.5339 14.4995 9.63105 15.3538 8.57836 16Z" fill="#12161A"/></g></svg>`,
       },
       {
         name: QuoteType.VerticalLine,
-        label: "Quote Type 2",
+        label: "Vertical Line",
         icon: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="editor-icon"><path id="Vector" fill-rule="evenodd" clip-rule="evenodd" d="M5 5V19H7V5H5ZM18.3198 7H9.68019L9.01941 10.3039L10.9806 10.6961L11.3198 9H13V15.5H11V17.5H17V15.5H15V9H16.6802L17.0194 10.6961L18.9806 10.3039L18.3198 7Z" fill="#12161A"/></g></svg>`,
       },
       {
         name: QuoteType.Box,
-        label: "Quote Type 3",
+        label: "Box",
         icon: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="editor-icon"><path id="Vector" fill-rule="evenodd" clip-rule="evenodd" d="M17 7H7V17H17V7ZM5 5V19H19V5H5Z" fill="#12161A"/></g></svg>`,
       },
     ];
   }
 
+  /**
+   * Renders the quote block element.
+   * @returns The HTML element representing the quote block.
+   */
   render(): HTMLElement {
     const container = make("div", [this._CSS.baseClass, this._CSS.wrapper]);
     this._quoteElement = make(
@@ -158,6 +190,10 @@ export default class Quote implements BlockTool {
     return container;
   }
 
+  /**
+   * Gets the current item in the quote block.
+   * @returns The HTML element representing the current item.
+   */
   get currentItem(): HTMLElement {
     let currentNode = window.getSelection()!.anchorNode;
 
@@ -168,6 +204,10 @@ export default class Quote implements BlockTool {
     return (currentNode as Element).closest(`.${this.CSS.text}`) as HTMLElement;
   }
 
+  /**
+   * Handles keydown events in the quote block.
+   * @param event - The keyboard event.
+   */
   handleKeydown(event: KeyboardEvent) {
     const currentItem = this._quoteElement;
 
@@ -193,11 +233,19 @@ export default class Quote implements BlockTool {
     }
   }
 
+  /**
+   * Exits the quote block and moves the caret to the next block.
+   */
   getOutOfQuote() {
     this.api.blocks.insert();
     this.api.caret.setToBlock(this.api.blocks.getCurrentBlockIndex());
   }
 
+  /**
+   * Gets the CSS class for the specified quote type.
+   * @param type - The type of the quote.
+   * @returns The CSS class name for the quote type.
+   */
   getTypeClass(type: QuoteType): string {
     switch (type) {
       case QuoteType.QuotationMark:
@@ -211,6 +259,11 @@ export default class Quote implements BlockTool {
     }
   }
 
+  /**
+   * Saves the quote data from the quote element.
+   * @param quoteElement - The HTML element representing the quote block.
+   * @returns The saved quote data.
+   */
   save(quoteElement: HTMLDivElement): QuoteData {
     const text = quoteElement.querySelector(`.${this._CSS.text}`);
 
@@ -227,6 +280,10 @@ export default class Quote implements BlockTool {
     };
   }
 
+  /**
+   * Renders the settings for the quote block.
+   * @returns An array of settings for the quote block.
+   */
   renderSettings(): HTMLElement | MenuConfig {
     return this.settings.map((item) => ({
       icon: item.icon || undefined,
@@ -237,23 +294,23 @@ export default class Quote implements BlockTool {
     }));
   }
 
+  /**
+   * Toggles the quote type.
+   * @param tune - The new type to set for the quote block.
+   */
   _toggleTune(tune: QuoteType) {
     this._data.type = tune;
 
-    // 타입 변경에 따라 클래스 업데이트
     if (this._quoteElement) {
-      // 기존 타입 클래스 제거
       this._quoteElement.classList.remove(
         "blockquote_type1",
         "blockquote_type2",
         "blockquote_type3"
       );
 
-      // 새 타입 클래스 추가
       this._quoteElement.classList.add(this.getTypeClass(tune));
     }
 
-    // 업데이트된 스타일을 반영하기 위해 블록을 갱신
     this._block.dispatchChange();
   }
 }
